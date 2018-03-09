a7c696bfac7fe5a84d32e66cc6593df485af8738
Gustavo Decker <gustavdz@gmail.com>
2018-03-09 09:58:01 -0500

    Ecuabill linked2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,80 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import {ContactPage} from "../pages/contact/contact";
+import {MenuPage} from "../pages/menu/menu";
+import {PopoverAboutPage} from "../pages/popover-about/popover-about";
+import {ProductsPage} from "../pages/products/products";
+
+
+import { RestProvider } from '../providers/rest/rest';
+
+Pro.init('79d9958c', {
+    appVersion: '0.0.1'
+})
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+    ionicErrorHandler: IonicErrorHandler;
+
+    constructor(injector: Injector) {
+        try {
+            this.ionicErrorHandler = injector.get(IonicErrorHandler);
+        } catch(e) {
+            // Unable to get the IonicErrorHandler provider, ensure
+            // IonicErrorHandler has been added to the providers list below
+        }
+    }
+
+    handleError(err: any): void {
+        Pro.monitoring.handleNewError(err);
+        // Remove this if you want to disable Ionic's auto exception handling
+        // in development mode.
+        this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+    }
+}
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ContactPage,
+    LoginPage,
+    MenuPage,
+    PopoverAboutPage,
+    ProductsPage,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ContactPage,
+    LoginPage,
+    MenuPage,
+    PopoverAboutPage,
+    ProductsPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }],
+    RestProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,App, LoadingController, ToastController, ActionSheetController, AlertController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
+import { LoginPage } from '../login/login';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,149 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    clients: any;
+    loading: any;
+    isLoggedIn: boolean = false;
 
-  }
+
+    constructor(public alertCtrl: AlertController, public app: App,public navCtrl: NavController, public restProvider: RestProvider,public loadingCtrl: LoadingController, private toastCtrl: ToastController, public actionSheetCtrl: ActionSheetController) {
+      console.log(localStorage.getItem("api_token"));
+      if(!localStorage.getItem("api_token")) {
+          navCtrl.setRoot(LoginPage);
+      }
+      if(localStorage.getItem("api_token")) {
+          this.isLoggedIn = true;
+          this.getClients();
+      }
+
+    }
+
+    showAddClient() {
+        let prompt = this.alertCtrl.create({
+            title: 'New Client',
+            message: "Enter the info for the new client",
+            inputs: [
+                {
+                    name: 'name',
+                    placeholder: 'Name'
+                },
+                {
+                    name: 'last_name',
+                    placeholder: 'Last Name'
+                },
+                {
+                    name: 'email',
+                    placeholder: 'Email',
+                    type: 'email'
+                },
+                {
+                    name: 'dni',
+                    placeholder: 'CI'
+                },
+                {
+                    name: 'phone',
+                    placeholder: 'Phone'
+                },
+                {
+                    name: 'address',
+                    placeholder: 'Address'
+                },
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        //console.log(data.name);
+                        this.restProvider.addClient(data).then(response => {
+                            console.log(response);
+                            this.getClients();
+                        });
+
+                    }
+                }
+            ]
+        });
+        prompt.present();
+    }
+
+    doClientRefresh(refresher) {
+        console.log('Begin async operation', refresher);
+        setTimeout(() => {
+            this.restProvider.getClients()
+                .then(data => {
+                    this.clients = data;
+                    console.log(this.clients);
+                    console.log('Async operation has ended');
+                    refresher.complete();
+                });
+        },500);
+    }
+
+    getClients() {
+        this.restProvider.getClients()
+        .then(data => {
+          this.clients = data;
+          console.log(this.clients);
+        });
+    }
+
+    showLoader(){
+        this.loading = this.loadingCtrl.create({
+            content: 'Authenticating...'
+        });
+
+        this.loading.present();
+    }
+
+    presentToast(msg) {
+        let toast = this.toastCtrl.create({
+            message: msg,
+            duration: 3000,
+            position: 'bottom',
+            dismissOnPageChange: true
+        });
+
+        toast.onDidDismiss(() => {
+            console.log('Dismissed toast');
+        });
+
+        toast.present();
+    }
+
+    moreOptions(client) {
+        let actionSheet = this.actionSheetCtrl.create({
+            title: 'Modify your Client',
+            buttons: [
+                {
+                    text: 'Destructive',
+                    role: 'destructive',
+                    handler: () => {
+                        console.log('Destructive clicked ' + JSON.stringify(client));
+                    }
+                },
+                {
+                    text: 'Archive',
+                    handler: () => {
+                        console.log('Archive clicked' + JSON.stringify(client));
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel clicked' + JSON.stringify(client));
+                    }
+                }
+            ]
+        });
+        actionSheet.present();
+    }
+
 
 }
